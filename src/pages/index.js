3fcfe0cbbdf4901d2d29888aaaff1e245edd95b2
mrcreel <michaelrcreel@gmail.com>
2018-09-11 11:01:37 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>2018 freeCodeCamp JAMstack Hackathon</h1>
+    <p>
+      I'm <a href="https://www.freecodecamp.org/mrcreel" target="_blank" >mrcreel</a> on freeCodeCamp
+    </p>
+    <p>Gimme my golden ticket!</p>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+  </div>
+)
+
+export default IndexPage
